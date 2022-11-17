dd44caa9e9e50cb233ad2905c66a2e846efb26a6
ffm5113 <73726304+ffm5113@users.noreply.github.com>
2022-11-16 21:39:51 -0800

    Update ArrayFunctionExercises.cpp

--- ArrayFunctionExercises.cpp
+++ ArrayFunctionExercises.cpp
@@ -8,12 +8,12 @@
 using namespace std;
 
 // Global Constant Initialization
-const string TITLE = "Array Function Exercises Program";
+const string TITLE = "Array Exercises Program";
 const string AUTHOR_LINE = "By Forrest Moulin";
 const int ARRAY_SIZE = 10;
 
 // Initialize global int array of size 10
-int numbers[ARRAY_SIZE] = {10,20,18,14,12,16,10,20,16,14};
+int numbers[ARRAY_SIZE] = {5,4,2,7,6,8,5,2,8,14};
 
 // Function prototypes to notify compiler
 void printOriginal(const int  numbersPassed[]);
@@ -37,13 +37,14 @@ int main()
 	// which is passed as const (read-only)
 	printOriginal(numbers);
 	squareEachNum(numbers);
+	addRandomNum(numbers);
 	addFollowingNum(numbers);
 	sumAllNums(numbers);
 	displayLargestNum(numbers);
 }
 void printOriginal(const int numbersPassed[])
 {	// Print original read-only array
-	cout << "Original array:" << endl
+	cout << "Original array values:" << endl
 		<<"{";
 	for (int i = 0; i < ARRAY_SIZE; i++)
 	{
@@ -58,7 +59,7 @@ void squareEachNum(const int numbersPassed[])
 {
 	cout << "Exercise # 1" << endl
 		<< setw(14) << setfill('-') << " " << endl
-		<< "Squared array:" << endl
+		<< "Squared array values:" << endl
 		<< "{";
 	// Make a array that will copy read only
 	int numbersCopy[ARRAY_SIZE];
@@ -76,10 +77,10 @@ void addRandomNum(const int numbersPassed[])
 {
 	cout << "Exercise # 2" << endl
 		<< setw(14) << setfill('-') << " " << endl
-		<< "Added random number array:" << endl
+		<< "Random number (0-10) added to array values:" << endl
 		<< "{";
-	// 10 options for random # (0-9) + 1 = 1-10
-	int randomNum = rand() % 10 + 1;
+	// 11 options for random # (0-10)
+	int randomNum = rand() % 10;
 	// Make a array that will copy read only
 	int numbersCopy[ARRAY_SIZE];
 	for (int i = 0; i < ARRAY_SIZE; i++)
@@ -89,6 +90,7 @@ void addRandomNum(const int numbersPassed[])
 		cout << numbersCopy[i];
 		placeCommas(i);
 	}
+	cout << "}" << endl << endl;
 }
 // Exercise 3: Adding the following int to the current int,
 // except for the last int
@@ -96,8 +98,8 @@ void addFollowingNum(const int numbersPassed[])
 {
 	cout << "Exercise # 3" << endl
 		<< setw(14) << setfill('-') <<  " " << endl
-		<< "Add the following number to each current number,"
-		<< endl << "except on the last number:" << endl << "{";
+		<< "The following number added to each number,"
+		<< endl << "except the last:" << endl << "{";
 	int numbersCopy[ARRAY_SIZE];
 	for (int i = 0; i < ARRAY_SIZE; i++)
 	{
@@ -127,7 +129,7 @@ void sumAllNums(const int numbersPassed[])
 	}
 	cout << sum << endl << endl;
 }
-// Exercise 5: Display the largest int of the array
+// Exercise 5: Find/display the largest int of the array
 void displayLargestNum(const int numbersPassed[]) {
 	cout << "Exercise # 5" << endl
 		<< setw(14) << setfill('-') << " " << endl
@@ -156,30 +158,35 @@ void placeCommas(int i)
 }
 /*
 * CONSOLE OUTPUT
-* Array Function Exercises Program
+* Array Exercises Program
 * By Forrest Moulin
-* 
+*
 * Original array:
-* {10,20,18,14,12,16,10,20,16,14}
-* 
+* {5,4,2,7,6,8,5,2,8,14}
+*
 * Exercise # 1
 * -------------
 * Squared array:
-* {100,400,324,196,144,256,100,400,256,196}
+* {25,16,4,49,36,64,25,4,64,196}
 * 
+* Exercise # 2
+* -------------
+* Random number (0-10) added to array values:
+* {8,7,5,10,9,11,8,5,11,17}
+*
 * Exercise # 3
 * -------------
 * Add the following number to each current number,
 * except on the last number:
-* {30,38,32,26,28,26,30,36,30,14}
-* 
+* {9,6,9,13,14,13,7,10,22,14}
+*
 * Exercise # 4
 * -------------
 * Sum of all array numbers:
-* 150
-* 
+* 61
+*
 * Exercise # 5
 * -------------
 * Largest of all original array numbers:
-* 20
+* 14
 */
